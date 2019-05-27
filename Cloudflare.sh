8082086c7227965fda1ada1a471c859293398bbf
Semyon Maryasin <simeon@maryasin.name>
2019-05-28 01:41:23 +0300

    use logfile in debug mode

--- Cloudflare.sh
+++ Cloudflare.sh
@@ -24,6 +24,8 @@ basedir=$(dirname "$0")
 
 attacked_file=$basedir/attacked
 
+[ "$debug" -eq 1 ] && exec > "${logfile:-$basedir/debug.log}"
+
 
 # You can put aforementioned config values either in-place
 # or in the file named 'config' in the script's directory.
